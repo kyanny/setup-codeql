3142691089b51c0ca2855a005d5b9c5ad64af48e
Kensuke Nagae <kyanny@gmail.com>
2021-09-15 01:39:07 +0900

    Add sample script to run CodeQL analysis to Go and Java projects (still not working)

---
+++ test-gojenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# http://f4bb1t.com/post/2020/12/14/codeql-for-golang-practise1/
+
+cd $HOME
+wget -nc https://golang.org/dl/go1.15.15.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
+
+cd $HOME/codeql-home
+git clone https://github.com/bndr/gojenkins
+cd gojenkins
+
+codeql database create codeql_database --language=go
+mkdir -p codeql-analysis
+codeql database analyze codeql_database ../codeql-go/ql/src/localReferences.ql --format=csv --output=codeql-analysis/go-results.csv
---
+++ test-struts_9805.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# https://hanatsuu.medium.com/codeql%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B-9648f32ab5c1
+
+sudo apt install maven -y
+
+cd $HOME/codeql-home
+git clone https://github.com/m-y-mo/struts_9805
+cd struts_9805
+
+codeql database create ./struts_db \
+  -j 0 -l java --command "mvn -B -DskipTests \
+  -DskipAssembly"
+
+codeql analyze struts_db \
+  ../codeql-repo/ql/java/ql/src/Security/CWE/CWE-502/UnsafeDeserialization.ql \
+  --format=csv --output=java-analysis/result.csv

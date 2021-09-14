c41b85d2c768f515d8078b5a918a6a11b6924d77
Kensuke Nagae <kyanny@gmail.com>
2021-09-15 01:40:07 +0900

    Use latest Go release

--- test-gojenkins.sh
+++ test-gojenkins.sh
@@ -4,7 +4,8 @@ set -e
 # http://f4bb1t.com/post/2020/12/14/codeql-for-golang-practise1/
 
 cd $HOME
-wget -nc https://golang.org/dl/go1.15.15.linux-amd64.tar.gz
+#wget -nc https://golang.org/dl/go1.15.15.linux-amd64.tar.gz
+wget -nc https://golang.org/dl/go1.17.1.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.17.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
